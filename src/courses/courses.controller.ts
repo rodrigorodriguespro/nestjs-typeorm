ec985161fb3b545c2b5a13633339689aec81e9ea
Rodrigo Rodrigues <prorodrigorodrigues@gmail.com>
2023-12-25 10:51:32 -0300

    ✨ feat: add update and delete router courses

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
+
   @Get()
-  findAll() {
-    return 'listagem de cursos'
+  findAll(@Res() response) {
+    return response.status(200).json({ menssage: 'lista de cursos' })
   }
 
   @Get(':id')
@@ -16,4 +17,16 @@ export class CoursesController {
   create(@Body() body){
     return body
   }
+
+  @Patch(':id')
+  update(@Param('id') id: String, @Body() body) {
+    return `Update course Id ${id}`
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  remove(@Param('id') id: String) {
+    return `Delete course Id ${id}`
+  }
+
 }
